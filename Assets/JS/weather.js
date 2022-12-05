5efb6310f14617877375345e340f7300b9596829
JagpreetRandio <115323708+JagpreetRandio@users.noreply.github.com>
2022-12-05 01:12:08 -0800

    started on JS

--- Assets/JS/weather.js
+++ Assets/JS/weather.js
@@ -16,4 +16,41 @@ var forecastTitle =document.querySelector("#forecast");
 var forecastContainer =document.querySelector("#fiveday-container");
 var pastSearchButton =document.querySelector("#past-buttons");
 
+//when searching a city
+var sumbitHandler = function(event){
+    event.preventDefault();
+    var city = cityInput.value.trim();
+    if(city){
+        // will get current weather
+        getCityWeather(city);
+        // and five day forecast
+        get5Day(city);
+        cities.unshift({city});
+        cityInput.value = "";
+    } else{
+        //an alert will show up if you have not entered in a city
+        alert("Please enter a City");
+    }
+    saveSearch();
+    pastSearch(city);
+}
+
+//search will be saved in the local storage
+var saveSearch = function(){
+    localStorage.setItem("cities", JSON.stringify(cities));
+};
+
+//for city weather, fetching the api 
+var getCityWeather = function(city){
+    var apiKey = "1dce1fbc0faf4a6477d89bb59d560269"
+    var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+    fetch(apiURL)
+    .then(function(response){
+        response.json().then(function(data){
+            displayWeather(data, city);
+        });
+    });
+};
+
 
