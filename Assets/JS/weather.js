e8e994470e382a3e8fc1e05147bc70771fdbb113
JagpreetRandio <115323708+JagpreetRandio@users.noreply.github.com>
2022-12-04 17:26:50 -0800

    added variables to JS

---
+++ Assets/JS/weather.js
@@ -0,0 +1,17 @@
+function startPage(){
+    // Added variables needed to develop code
+    var enterCity = document.getElementById("enterCity");
+    var cityName = document.getElementById("city-name");
+    var searchBtn = document.getElementById("search-button");
+    var clearHistory = document.getElementById("clearHistory");
+    var history = document.getElementById("history")
+    var weatherPic = document.getElementById("weather-pic");
+    var curTemp =document.getElementById("temperature");
+    var curHum = document.getElementById("humidity");
+    var curWind = document.getElementById("wind");
+    var uvIndex = document.getElementById("UV-index");
+    var fiveDay = document.getElementById("fiveday");
+    var today = document.getElementById("today")
+    
+
+}
